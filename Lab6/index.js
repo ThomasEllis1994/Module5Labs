5f9f28c9caa3a7e76169b4606023401d6b72c442
ThomasEllis1994 <ThomasGEllis004@gmail.com>
2024-06-08 17:31:32 -0400

    trying to solve lab 6

--- Lab6/index.js
+++ Lab6/index.js
@@ -1,7 +1,23 @@
 import app from "./app.js";
 const port = 3000;
+// import express from "express";
+// app.use(express.json);
 // start the app to listen on the right port
 app.listen(port, () => {
     console.log(`Example app listening at
 http://localhost:${port}`);
 });
+
+// import express from "express";
+// import calculatorRoutes from "./routes/calculatorRoutes.js";
+
+// const app = express();
+// const port = 3000;
+// app.use(express.json());
+
+// app.use("/", express.static("public"));
+// app.use("/calculator", calculatorRoutes);
+
+// app.listen(port, () => {
+//     console.log(`example app listening at http://localhost:${port}`);
+// });
