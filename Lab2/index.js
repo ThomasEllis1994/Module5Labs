03be663cd9889cce340ce4bb6c20916fa6b6fe7a
ThomasEllis1994 <ThomasGEllis004@gmail.com>
2024-06-08 15:06:36 -0400

    fixed lab 2

--- Lab2/index.js
+++ Lab2/index.js
@@ -4,15 +4,10 @@ import calculatorRoutes from "./routes/calculatorRoutes.js";
 const app = express();
 const port = 3000;
 app.use(express.json());
-//Lab 1 creating second port
-const port2 = 4660;
 
 app.use("/", express.static("public"));
 app.use("/calculator", calculatorRoutes);
 
 app.listen(port, () => {
     console.log(`example app listening at http://localhost:${port}`);
-});
-app.listen(port2, () => {
-    console.log(`This one is listening at http://localhost:${port2} `);
 });
